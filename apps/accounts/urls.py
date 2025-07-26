abb78fdd3548ec367c310c2fdabdc5c6e3fe8e02
angelchiav <achiavicuna@gmail.com>
2025-07-26 13:52:06 -0400

    Add JWT auth and registration endpoints
    Added user registration, login, and token refresh endpoints to accounts app using SimpleJWT. Also removed comments from ASGI config for clarity.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, UserProfileViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import UserRegistrationView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -8,4 +13,7 @@ router.register(r'profiles', UserProfileViewSet, basename='userprofile')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
